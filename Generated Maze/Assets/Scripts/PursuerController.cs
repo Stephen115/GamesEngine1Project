65a8c9dbd97d5fabc7b281bf57ae2bf30cbba687
Stephen115 <47150698+Stephen115@users.noreply.github.com>
2020-12-24 02:26:45 +0000

    Pathfinding Leap forward
    The maze now correctly loads in it's structure, and the tentacle monster can find the player, now I just need to get it moving, work considerably slowed by a broken mouse. There is also now a nice skybox implemented, and I'm thinking about bringing back the rainbow walls or something slightly see through so that you can be terrified of the ever approaching monster.

--- Generated Maze/Assets/Scripts/PursuerController.cs
+++ Generated Maze/Assets/Scripts/PursuerController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 public class PursuerController : MonoBehaviour
 {
     public Transform target;
- //TheGrapIsReady() will be executed when the gameObject receives the "TheGraphIsReady" message
+    //TheGrapIsReady() will be executed when the gameObject receives the "TheGraphIsReady" message
     public void TheGraphIsReady()
     {
         gameObject.GetComponent<Pursuer>().MoveTo(target);
     }
+
+    void Start()
+    {
+        Invoke("TheGraphIsReady", 30.0f);
+    }
+
 }
